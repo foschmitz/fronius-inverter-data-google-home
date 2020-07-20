7cf15c2c2710558809283682e1b7e7c0557ac4d5
Frank Schmitz <frank.schmitz@rewe-digital.com>
2020-07-20 09:19:16 +0200

    first api calls

--- app.js
+++ app.js
@@ -14,10 +14,12 @@ const request = require('request');
 var express = require("express");
 var app = express();
 
-var power_from_battery = null;
-var power_from_grid = null;
-var power_from_pv = null;
-var power_usage = null;
+var pv_data = {
+  power_from_battery : null,
+  power_from_grid : null,
+  power_from_pv : null,
+  power_usage : null
+};
 
 // schedule tasks to poll the inverters
 cron.schedule("* * * * *", function() {
@@ -25,13 +27,13 @@ cron.schedule("* * * * *", function() {
   update_data();
 });
 
-app.listen(3001, () => {
- console.log("Server running on port 3001");
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
 });
 
 
 app.get("/inverter-data", (req, res, next) => {
- res.json({"power_from_battery":power_from_battery,"power_from_grid":power_from_grid,"power_from_pv":power_from_pv,"power_usage":power_usage});
+ res.json(pv_data);
 });
 
 app.get("/update-data", (req, res, next) => {
@@ -42,12 +44,15 @@ app.get("/update-data", (req, res, next) => {
 async function update_data() {
   console.log("updating data...")
   try {
-        const symo = await get_local_inverter_data('http://192.168.1.84/solar_api/v1/GetPowerFlowRealtimeData.fcgi')
-        const primo = await get_local_inverter_data('http://192.168.1.85/solar_api/v1/GetPowerFlowRealtimeData.fcgi')
-        power_from_pv = symo.Body.Data.Site.P_PV + primo.Body.Data.Site.P_PV;
-        power_from_battery = symo.Body.Data.Site.P_Akku;
-        power_from_grid = symo.Body.Data.Site.P_Grid;
-        power_usage = power_from_grid + power_from_pv; // this will be wrong when the battery is charging, need to find the data from the smart meter
+        const symo = await get_local_inverter_data('http://192.168.1.84/solar_api/v1/GetPowerFlowRealtimeData.fcgi');
+        const primo = await get_local_inverter_data('http://192.168.1.85/solar_api/v1/GetPowerFlowRealtimeData.fcgi');
+        const powermeter = await get_local_inverter_data('http://192.168.1.84/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceId=0');
+        pv_data.power_from_pv = symo.Body.Data.Site.P_PV + primo.Body.Data.Site.P_PV;
+        pv_data.power_from_battery = symo.Body.Data.Site.P_Akku;
+        pv_data.power_from_grid = symo.Body.Data.Site.P_Grid;
+        pv_data.power_usage = pv_data.power_from_pv + powermeter.Body.Data.PowerReal_P_Sum;
+        console.log(pv_data);
+
     } catch (error) {
         console.error('ERROR:',error);
     }
